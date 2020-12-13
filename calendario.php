9fc587bf4d306a2a9cc7f225704e4fb0bcc95739
Alex <jalopez9722@misena.edu.co>
2020-12-13 13:22:55 -0500

    Se prepara el calendario con consultas a la base de datos notas importantes leer para el desarrollador

--- calendario.php
+++ calendario.php
@@ -1,4 +1,4 @@
-<!-- http://localhost/conferenciaPHP/calendario.php -->
+<!-- http://localhost:81/Conferencia_PHP/calendario.php -->
 <?php include_once('includes/templates/header.php'); ?>
 <section class="seccion contenedor">
     <h2>Calendario de Eventos</h2>
@@ -6,7 +6,7 @@
     <?php
     try {
         require_once 'includes/funciones/bd_conexion.php';
-        $sql = "SELECT evento_id, nombre_evento, fecha_evento, hora_evento, cat_evento, 
+        $sql = "SELECT evento_id, nombre_evento, fecha_evento, hora_evento, cat_evento, icono, 
         nombre_invitado, apellido_invitado ";
         $sql .= " FROM eventos ";
         $sql .= " INNER JOIN categoria_evento ";
@@ -22,353 +22,70 @@
     ?>     
 
     <div class="calendario">
-        
+
         <?php
-        
         $calendario = array();
-       
+
         while ($eventos = $resultado->fetch_assoc()) {
 
-        //obtener fecha 
-        
-        $fecha = $eventos['fecha_evento'];
-        $categorias = $eventos ['cat_evento'];
+            //obtener fecha 
+
+            $fecha = $eventos['fecha_evento'];
+            $categorias = $eventos ['cat_evento'];
 
 
             $evento = array(
-                'nombre_evento' => $eventos ['nombre_evento'],
+                'titulo' => $eventos ['nombre_evento'],
                 'fecha' => $eventos ['fecha_evento'],
-                'hora' => $eventos ['hora_evento'] ,
+                'hora' => $eventos ['hora_evento'],
                 'categoria' => $eventos ['cat_evento'],
-                'invitado' => $eventos ['nombre_invitado']."".['apellido_invitado']
+                'icono' => 'fa'.' '.$eventos ['icono'],
+                'invitado' => $eventos['nombre_invitado']." ".$eventos['apellido_invitado']
             );
             $calendario[$fecha][] = $evento;
             ?>
         <?php } //cierre while   ?> 
 
 
-        <?php  
+        <?php
         //imprime todos los eventos   
-            foreach ($calendario as $dia => $lista_eventos) { ?>
+        foreach ($calendario as $dia => $lista_eventos) {
+            ?>
             <h3>
                 <i class="fa fa-calendar"> </i>  
-                    <?php 
-                        echo $dia; 
-                    ?>        
+
+                <?php
+                setlocale(LC_TIME, 'spanish.UTF-8');
+                echo strftime("%A, %d de %B del %Y", strtotime($dia));
+                ?>        
             </h3>
 
 
-        <?php } //cierre foreach?>    
- 
-            <pre> 
-                <?php var_dump($calendario); ?> 
+            <?php foreach ($lista_eventos as $evento) { ?>
 
+                <div class="dia">
+                    <p class="titulo"> <?php echo $evento['titulo']; ?> </p>
+                    <p> <i class="fa fa-clock-o" aria-hidden="TRUE">  </i>
+                        <?php echo $evento['fecha'] . " " . $evento['hora'] ?> 
+                    </p>
+                    <p> 
+                        <i class="<?php echo $evento['icono']; ?>" aria-hidden="TRUE"></i>
+                        <?php echo $evento['categoria'] ?> 
+                        
+                    </p>
+                    <p> 
+                        <i class="fa fa-user" aria-hidden="TRUE"></i>
+                        <?php echo $evento['invitado']  ?>
+                    </p>    
 
-            </pre>
-                <?php echo "hola aqui estoy";  ?>
+                </div>
+
+            <?php } //cierre foreach  eventos  ?> 
+        <?php } //cierre foreach  dia ?>    
+
         <?php $conn->close(); ?>
 
     </div>
     <!--.calendario-->
 </section>
 <?php include_once('includes/templates/footer.php') ?> 
-
-
-<!-- CODIGO CALENDARIO -----
-
-
-        <h3>
-            <i class="fa fa-calendar" aria-hidden="true"></i>
-            2016-12-09                                  </h3>
-
-        <div class="dia">
-            <p class="titulo">HTML5 y CSS3</p>
-            <p class="hora"><i class="fa fa-clock-o" aria-hidden="true"></i> 2020-12-09 02:00:00 hrs
-            <p>
-
-                <i class="fa fa-code" aria-hidden="true"></i> Taller </p>
-            <p><i class="fa fa-user" aria-hidden="true"></i> Juan Sanchez </p>
-
-        </div>
-
-
-
-        <div class="dia">
-            <p class="titulo">WordPress</p>
-            <p class="hora"><i class="fa fa-clock-o" aria-hidden="true"></i> 2020-12-09 19:00:00 hrs
-            <p>
-
-                <i class="fa fa-code" aria-hidden="true"></i> Taller </p>
-            <p><i class="fa fa-user" aria-hidden="true"></i> Harold Garcia </p>
-
-        </div>
-
-
-
-        <div class="dia">
-            <p class="titulo">Como ser freelancer</p>
-            <p class="hora"><i class="fa fa-clock-o" aria-hidden="true"></i> 2020-12-09 10:00:00 hrs
-            <p>
-
-                <i class="fa fa-comment" aria-hidden="true"></i> Conferencias </p>
-            <p><i class="fa fa-user" aria-hidden="true"></i> Susan Sanchez </p>
-
-        </div>
-
-
-
-        <div class="dia">
-            <p class="titulo">TecnologÃ­as del Futuro PHP</p>
-            <p class="hora"><i class="fa fa-clock-o" aria-hidden="true"></i> 2020-12-09 05:00:00 hrs
-            <p>
-
-                <i class="fa fa-comment" aria-hidden="true"></i> Conferencias </p>
-            <p><i class="fa fa-user" aria-hidden="true"></i> Juan Sanchez </p>
-
-        </div>
-
-
-
-        <div class="dia">
-            <p class="titulo">Seguridad en la Web</p>
-            <p class="hora"><i class="fa fa-clock-o" aria-hidden="true"></i> 2020-12-09 07:00:00 hrs
-            <p>
-
-                <i class="fa fa-comment" aria-hidden="true"></i> Conferencias </p>
-            <p><i class="fa fa-user" aria-hidden="true"></i> Shari Herrera </p>
-
-        </div>
-
-
-
-        <div class="dia">
-            <p class="titulo">DiseÃ±o UI y UX para mÃ³viles</p>
-            <p class="hora"><i class="fa fa-clock-o" aria-hidden="true"></i> 2020-12-09 10:00:00 hrs
-            <p>
-
-                <i class="fa fa-university" aria-hidden="true"></i> Seminarios </p>
-            <p><i class="fa fa-user" aria-hidden="true"></i> Susan Sanchez </p>
-
-        </div>
-
-
-        <h3>
-            <i class="fa fa-calendar" aria-hidden="true"></i>
-            2016-12-10                                  </h3>
-
-        <div class="dia">
-            <p class="titulo">Angular 5</p>
-            <p class="hora"><i class="fa fa-clock-o" aria-hidden="true"></i> 2020-12-10 19:00:00 hrs
-            <p>
-
-                <i class="fa fa-university" aria-hidden="true"></i> Seminarios </p>
-            <p><i class="fa fa-user" aria-hidden="true"></i> Juan Sanchez </p>
-
-        </div>
-
-
-
-        <div class="dia">
-            <p class="titulo">PHP y MySQL</p>
-            <p class="hora"><i class="fa fa-clock-o" aria-hidden="true"></i> 2020-12-10 12:00:00 hrs
-            <p>
-
-                <i class="fa fa-code" aria-hidden="true"></i> Taller </p>
-            <p><i class="fa fa-user" aria-hidden="true"></i> Shari Herrera </p>
-
-        </div>
-
-
-
-        <div class="dia">
-            <p class="titulo">JavaScript Avanzado</p>
-            <p class="hora"><i class="fa fa-clock-o" aria-hidden="true"></i> 2016-12-10 14:00:00 hrs
-            <p>
-
-                <i class="fa fa-code" aria-hidden="true"></i> Taller </p>
-            <p><i class="fa fa-user" aria-hidden="true"></i> Juan Sanchez </p>
-
-        </div>
-
-
-
-        <div class="dia">
-            <p class="titulo">SEO en Google</p>
-            <p class="hora"><i class="fa fa-clock-o" aria-hidden="true"></i> 2020-12-10 17:00:00 hrs
-            <p>
-
-                <i class="fa fa-code" aria-hidden="true"></i> Taller </p>
-            <p><i class="fa fa-user" aria-hidden="true"></i> Susana Rivera </p>
-
-        </div>
-
-
-
-        <div class="dia">
-            <p class="titulo">De Photoshop a HTML5 y CSS3</p>
-            <p class="hora"><i class="fa fa-clock-o" aria-hidden="true"></i> 2020-12-10 19:00:00 hrs
-            <p>
-
-                <i class="fa fa-code" aria-hidden="true"></i> Taller </p>
-            <p><i class="fa fa-user" aria-hidden="true"></i> Harold Garcia </p>
-
-        </div>
-
-
-
-        <div class="dia">
-            <p class="titulo">PHP Intermedio y Avanzado</p>
-            <p class="hora"><i class="fa fa-clock-o" aria-hidden="true"></i> 2020-12-10 21:00:00 hrs
-            <p>
-
-                <i class="fa fa-code" aria-hidden="true"></i> Taller </p>
-            <p><i class="fa fa-user" aria-hidden="true"></i> Susan Sanchez </p>
-
-        </div>
-
-
-
-        <div class="dia">
-            <p class="titulo">Como crear una tienda online que venda millones en pocos dÃ­a</p>
-            <p class="hora"><i class="fa fa-clock-o" aria-hidden="true"></i> 2020-12-10 10:00:00 hrs
-            <p>
-
-                <i class="fa fa-comment" aria-hidden="true"></i> Conferencias </p>
-            <p><i class="fa fa-user" aria-hidden="true"></i> Susan Sanchez </p>
-
-        </div>
-
-
-
-        <div class="dia">
-            <p class="titulo">Los mejores lugares para encontrar trabajo</p>
-            <p class="hora"><i class="fa fa-clock-o" aria-hidden="true"></i> 2020-12-10 17:00:00 hrs
-            <p>
-
-                <i class="fa fa-comment" aria-hidden="true"></i> Conferencias </p>
-            <p><i class="fa fa-user" aria-hidden="true"></i> Rafael Bautista </p>
-
-        </div>
-
-
-
-        <div class="dia">
-            <p class="titulo">Pasos para crear un negocio rentable </p>
-            <p class="hora"><i class="fa fa-clock-o" aria-hidden="true"></i> 2020-12-10 19:00:00 hrs
-            <p>
-
-                <i class="fa fa-comment" aria-hidden="true"></i> Conferencias </p>
-            <p><i class="fa fa-user" aria-hidden="true"></i> Shari Herrera </p>
-
-        </div>
-
-
-
-        <div class="dia">
-            <p class="titulo">DiseÃ±o UI y UX para mÃ³viles</p>
-            <p class="hora"><i class="fa fa-clock-o" aria-hidden="true"></i> 2020-12-10 17:00:00 hrs
-            <p>
-
-                <i class="fa fa-university" aria-hidden="true"></i> Seminarios </p>
-            <p><i class="fa fa-user" aria-hidden="true"></i> Harold Garcia </p>
-
-        </div>
-
-
-        <h3>
-            <i class="fa fa-calendar" aria-hidden="true"></i>
-            2016-12-11                                  </h3>
-
-        <div class="dia">
-            <p class="titulo">Laravel</p>
-            <p class="hora"><i class="fa fa-clock-o" aria-hidden="true"></i> 2020-12-11 10:00:00 hrs
-            <p>
-
-                <i class="fa fa-code" aria-hidden="true"></i> Taller </p>
-            <p><i class="fa fa-user" aria-hidden="true"></i> Rafael Bautista </p>
-
-        </div>
-
-
-
-        <div class="dia">
-            <p class="titulo">Crea tu propia API</p>
-            <p class="hora"><i class="fa fa-clock-o" aria-hidden="true"></i> 2020-12-11 12:00:00 hrs
-            <p>
-
-                <i class="fa fa-code" aria-hidden="true"></i> Taller </p>
-            <p><i class="fa fa-user" aria-hidden="true"></i> Shari Herrera </p>
-
-        </div>
-
-
-
-        <div class="dia">
-            <p class="titulo">JavaScript y jQuery</p>
-            <p class="hora"><i class="fa fa-clock-o" aria-hidden="true"></i> 2020-12-11 14:00:00 hrs
-            <p>
-
-                <i class="fa fa-code" aria-hidden="true"></i> Taller </p>
-            <p><i class="fa fa-user" aria-hidden="true"></i> Juan Sanchez </p>
-
-        </div>
-
-
-
-        <div class="dia">
-            <p class="titulo">Creando Plantillas para WordPress</p>
-            <p class="hora"><i class="fa fa-clock-o" aria-hidden="true"></i> 2020-12-11 17:00:00 hrs
-            <p>
-
-                <i class="fa fa-code" aria-hidden="true"></i> Taller </p>
-            <p><i class="fa fa-user" aria-hidden="true"></i> Susana Rivera </p>
-
-        </div>
-
-
-
-        <div class="dia">
-            <p class="titulo">Tiendas Virtuales en Magento</p>
-            <p class="hora"><i class="fa fa-clock-o" aria-hidden="true"></i> 2020-12-11 19:00:00 hrs
-            <p>
-
-                <i class="fa fa-code" aria-hidden="true"></i> Taller </p>
-            <p><i class="fa fa-user" aria-hidden="true"></i> Harold Garcia </p>
-
-        </div>
-
-
-
-        <div class="dia">
-            <p class="titulo">Creando una App en Android en una maÃ±ana</p>
-            <p class="hora"><i class="fa fa-clock-o" aria-hidden="true"></i> 2020-12-11 10:00:00 hrs
-            <p>
-
-                <i class="fa fa-university" aria-hidden="true"></i> Seminarios </p>
-            <p><i class="fa fa-user" aria-hidden="true"></i> Susana Rivera </p>
-
-        </div>
-
-
-
-        <div class="dia">
-            <p class="titulo">Creando una App en iOS en una tarde</p>
-            <p class="hora"><i class="fa fa-clock-o" aria-hidden="true"></i> 2020-12-11 17:00:00 hrs
-            <p>
-
-                <i class="fa fa-university" aria-hidden="true"></i> Seminarios </p>
-            <p><i class="fa fa-user" aria-hidden="true"></i> Rafael Bautista </p>
-
-        </div>
-
-
-
-        <div class="dia">
-            <p class="titulo">Flexbox para principiantes</p>
-            <p class="hora"><i class="fa fa-clock-o" aria-hidden="true"></i> 2020-12-10 11:00:00 hrs
-            <p>
-
-                <i class="fa fa-comment" aria-hidden="true"></i> Conferencias </p>
-            <p><i class="fa fa-user" aria-hidden="true"></i> Susana Rivera </p>
-
-        </div> -->
--- includes/templates/header.php
+++ includes/templates/header.php
@@ -10,6 +10,7 @@
 
     <link rel="apple-touch-icon" href="apple-touch-icon.png">
     <!-- Place favicon.ico in the root directory -->
+    <link rel="shortcut icon" href="images/Favicon.ico">   
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
